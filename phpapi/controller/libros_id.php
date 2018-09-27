62ecc6591f8e1b4689d1d736fed55ee1b5e3841d
Luis Dillon <ldillon83@gmail.com>
2018-09-27 13:26:16 +0200

    Explicacion de autenticacion en /libros{id} case DELETE

--- phpapi/controller/libros_id.php
+++ phpapi/controller/libros_id.php
@@ -1,7 +1,7 @@
 <?php
 include_once(__DIR__."/../../autoload.php");
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+//header("Content-Type: application/json; charset=UTF-8");
 
 use Phpapi\Model\Libro;
 
@@ -49,6 +49,15 @@ switch($metodo){
             echo json_encode($respuesta);
         break;
         case "DELETE":
+            //gráfico de autenticación básica
+            //https://docs.microsoft.com/es-es/aspnet/web-api/overview/security/basic-authentication/_static/image1.png
+            //asumir la sgte cabecera 'Authorization: Basic QWxhZGRpbjpPcGVuU2VzYW1l' 
+            //obtener el texto después de Basic (token) y realizar un base64_decode
+            //
+            //si falla la autenticación, enviar las siguientes cabeceras:
+            //header('WWW-Authenticate: Basic realm="Mi dominio"');
+            //header('HTTP/1.0 401 Unauthorized');
+
             $id = filter_var($_GET["id"], FILTER_SANITIZE_NUMBER_INT);
             $libro = new Libro();
             if(!$libro->cargar($id)){
